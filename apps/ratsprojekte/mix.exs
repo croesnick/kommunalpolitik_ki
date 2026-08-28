14c1efdb2b70344551b8e1cc619a1a8d38b7d479
Carsten Rösnick-Neugebauer <croesnick@gmail.com>
2026-08-28 18:07:07 +0000

    chore(deps): update dependency anubis_mcp to v2

--- apps/ratsprojekte/mix.exs
+++ apps/ratsprojekte/mix.exs
@@ -41,7 +41,7 @@ defmodule Ratsprojekte.MixProject do
       {:jason, "~> 1.2"},
       {:bandit, "~> 1.5"},
       {:tidewave, "~> 0.6", only: :dev},
-      {:anubis_mcp, "~> 1.6", only: [:dev, :test]},
+      {:anubis_mcp, "~> 2.0", only: [:dev, :test]},
       {:lazy_html, ">= 0.1.0", only: [:dev, :test]},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
